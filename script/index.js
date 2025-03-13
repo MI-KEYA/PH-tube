7084beaa70fbeb216b6aa701dbfa06221f7ee110
MI-KEYA <moniakeya913@gmail.com>
2025-03-14 02:15:37 +0600

    added first page

---
+++ script/index.js
@@ -0,0 +1,71 @@
+
+function loadCategories() {
+    fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+        .then((res) => res.json())
+        .then((data) => displayCategories(data.categories));
+}
+
+function loadVideos() {
+    fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+        .then((res) => res.json())
+        .then((data) => displayVideos(data.videos))
+}
+
+//{ category
+// : 
+// "Music"
+// category_id
+// : 
+// "1001"}
+function displayCategories(categories) {
+    const categoryContainer = document.getElementById("category-container");
+
+    for (let cat of categories) {
+
+        const categoryDiv = document.createElement("div");
+        categoryDiv.innerHTML = `
+        <button class="btn btn-sm hover:text-white hover:bg-[#FF1F3D] ">${cat.category}</button>
+        `;
+        categoryContainer.append(categoryDiv)
+
+    }
+}
+const displayVideos = (videos) => {
+    const videoContainer = document.getElementById("video-container");
+
+    videos.forEach(video => {
+        // console.log(video)
+
+        const videoCard = document.createElement("div");
+        videoCard.innerHTML = `
+            <div class="card bg-base-100">
+            <figure class="relative">
+                <img class= "w-full h-[150px] object-cover"
+                src="${video.thumbnail}"
+                alt="Shoes" />
+                <span class="absolute bottom-2 right-2 text-white bg-black 
+                rounded-sm px-2 text-sm">3hrs 56 min ago</span>
+            </figure>
+            <div class=" flex gap-3 px-0 py-5">
+                <div class="profile">
+                    <div class="avatar">
+                        <div class="ring-primary ring-offset-base-100 w-6 rounded-full ring ring-offset-2">
+                          <img src="${video.authors[0].profile_picture}" />
+                        </div>
+                      </div>
+                </div>
+                <div class="intro">
+                    <h2 class="text-sm font-semibold">${video.title}</h2>
+                    <p class="text-sm text-gray-400 flex gap-1">${video.authors[0].profile_name}
+                        <img class="w-5 h-5" src="https://img.icons8.com/?size=96&id=98A4yZTt9abw&format=png" alt="">
+                    </p>
+                    <p class="text-sm text-gray-400 flex gap-1">${video.others.views}</p>
+                </div>
+            </div>
+            </div>
+        `;
+        videoContainer.append(videoCard)
+    })
+}
+
+loadCategories();
